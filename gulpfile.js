347b26114d8dbdf5219c10986ab7224fad9bcdea
Matt Greenberg <magreenberg@fullsail.edu>
2017-09-17 02:46:44 -0600

    setup default and scss gulp task

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Package Declerations
+var gulp           = require('gulp'),
+	sass           = require('gulp-sass'),
+	browserSync    = require('browser-sync').create(),
+	postcss        = require('gulp-postcss'),
+	autoprefixer   = require('autoprefixer');
+
+// Default Task
+gulp.task('default', ()=>{
+
+});
+
+// Process SCSS
+gulp.task('scss', ()=>{
+
+	// Auto Prefixer browser list
+	var processors = [
+		autoprefixer({ browsers: ['last 2 versions'] })
+	];
+	// Process SCSS files 
+	return gulp.src('./scss/*.scss')
+	.pipe(sass())
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
+
+});
